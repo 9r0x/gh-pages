c1f43ece59fa66c163423f9c49d1678679147690
Shukai Ni <nishukai@hotmail.com>
2019-05-30 00:16:26 +0800

    Site updated: 2019-05-30 00:16:26

---
+++ Seichi/precache-manifest.a2b1acbfb31bca4439b92e6d2fdfba13.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2ba8b06465e21ea355be5a49b88541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "144f05a378df76dbab91",
+    "url": "/static/js/2.58ae5a34.chunk.js"
+  },
+  {
+    "revision": "95e1b4670f9f430f2d2a",
+    "url": "/static/js/main.e975c458.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
